9361a1242526e468b8d3a7eb60764dd437681567
bi3mer <colan@comcast.net>
2022-02-07 15:12:35 -0500

    tree search now works as intended with not allowing empty links if the configuration requires that

--- LinkGeneration/TreeSearch.py
+++ LinkGeneration/TreeSearch.py
@@ -4,16 +4,14 @@ def build_link(start, end, config):
     if config.ALLOW_EMPTY_LINK and \
        config.level_is_valid(start + end) and \
        config.get_percent_playable(start + end) == 1.0:
-
         return []
 
     for START_LINK in config.FORWARD_STRUCTURE_GRAM.get_output(start):
         for END_LINK in config.BACKWARD_STRUCTURE_GRAM.get_output(end):
             END_LINK = list(reversed(END_LINK))
 
-            assert start + START_LINK + END_LINK + end
-
-            if config.get_percent_playable(start + START_LINK + END_LINK + end) == 1.0:
+            if config.ALLOW_EMPTY_LINK and \
+                config.get_percent_playable(start + START_LINK + END_LINK + end) == 1.0:
                 return START_LINK + END_LINK
 
             output = config.LINKERS

9fde165b5ce57941e18a43297c1f0bd7e0d116e5
bi3mer <colan@comcast.net>
2022-06-17 14:15:40 -0400

    prepping for new run with stamina clamped

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 screen -dm bash -c "pypy3 main.py --generate-links --dungeongram --gram-elites --allow-empty-link"
 screen -dm bash -c "pypy3 main.py --generate-links --dungeongram --gram-elites --no-empty-link"
-screen -dm bash -c "pypy3 main.py --generate-links --icarus --gram-elites --allow-empty-link"
-screen -dm bash -c "pypy3 main.py --generate-links --mario --gram-elites --allow-empty-link"
+# screen -dm bash -c "pypy3 main.py --generate-links --icarus --gram-elites --allow-empty-link"
+# screen -dm bash -c "pypy3 main.py --generate-links --mario --gram-elites --allow-empty-link"
